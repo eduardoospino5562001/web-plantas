e5bfecd4b574e7ecd6d003d8cf827ac36069c7ff
Eduardo <eduardoospino5562001@gmail.com>
2025-11-23 00:09:11 -0500

    arreglando errores de limpieza

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle, ArrowRight, ShieldCheck, Zap, MapPin, Globe, Activity } from 'lucide-react';
+import { CheckCircle, ArrowRight, Zap, Globe, Activity } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 interface Props {
